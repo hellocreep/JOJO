8e9d31872775d586bf6d7d0bf5c0547150e805cd
hellocreep <hellocreep@gmail.com>
2014-06-29 17:54:05 +0800

    add Star platinum

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ require.config({
     /* Vendor */
     raphael: 'vendor/raphael',
     underscore: 'vendor/underscore',
+    jquery: 'vendor/jquery.min',
 
     /* Modules */
     stand: 'stand/stand.min'
@@ -13,21 +14,42 @@ require.config({
   }
 });
 
-require(['stand'], function(Stand) {
+require(['jquery', 'stand'], function($, Stand) {
+$(function() {
+  // Stand.set({
+  //   maxLevle: 5
+  // });
+
   var stand = new Stand({
     r: 150,
+    height: 600,
+    width: 600,
     centerX: 300,
     centerY: 300,
     data: {
+      theme: '#aa4095',
+      standName: '',
+      masterName: '',
       status: {
         '破坏力': 5,
         '速度': 5,
-        '射程距离': 5,
+        '射程距离': 3,
         '持久力': 5,
         '精密动作性': 5,
-        '成长性': 4
+        '成长性': 5
       }
     }
-  })
+  });
+
+
+  // stand.update({
+  //   standName: '',
+  //   masterName: '',
+  //   status: {
+
+  //   }
+  // });
+
+});
 });
 
