70d80b1b87b8f52eb5893e7a54d9de63a0215b3c
hellocreep <hellocreep@gmail.com>
2014-07-06 20:51:47 +0800

    Update data

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,13 +4,22 @@ module.exports = function(grunt) {
     copy: {
       main: {
         files: [
-          {expand: true, cwd: '<%=bowerDir%>/bootstrap/dist/css/', src: 'bootstrap.min.css', dest: 'css/'},
+          {expand: true, cwd: '<%=bowerDir%>/pure/', src: 'pure.css', dest: 'css/'},
           {expand: true, cwd: '<%=bowerDir%>/jquery/dist/', src: 'jquery.min.js', dest: 'js/vendor/'},
           {expand: true, cwd: '<%=bowerDir%>/modernizr/', src: 'modernizr.js', dest: 'js/vendor/'},
           {expand: true, cwd: '<%=bowerDir%>/raphael/', src: 'raphael.js', dest: 'js/vendor/'},
           {expand: true, cwd: '<%=bowerDir%>/requirejs/', src: 'require.js', dest: 'js/vendor/'},
           {expand: true, cwd: '<%=bowerDir%>/underscore/', src: 'underscore.js', dest: 'js/vendor/'}
         ]
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          hostname: 'localhost',
+          keepalive: true
+        }
       }
     }
   });
--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ require.config({
     jquery: 'vendor/jquery.min',
 
     /* Modules */
-    stand: 'stand/stand.min'
+    stand: 'stand/dist/stand'
   },
   shim: {
 
@@ -16,39 +16,54 @@ require.config({
 
 require(['jquery', 'stand'], function($, Stand) {
 $(function() {
-  // Stand.set({
-  //   maxLevle: 5
-  // });
+  var stand;
 
-  var stand = new Stand({
-    r: 150,
-    height: 600,
-    width: 600,
-    centerX: 300,
-    centerY: 300,
-    data: {
-      theme: '#aa4095',
-      standName: '',
-      masterName: '',
-      status: {
-        '破坏力': 5,
-        '速度': 5,
-        '射程距离': 3,
-        '持久力': 5,
-        '精密动作性': 5,
-        '成长性': 5
-      }
-    }
-  });
-
-
-  // stand.update({
-  //   standName: '',
-  //   masterName: '',
-  //   status: {
-
+  // $('#mask').on('mousemove', function(e) {
+  //   if(e.offsetX < 41) {
+  //     mask.fillStyle = 'transparent';
   //   }
   // });
+
+  // mask = $('#mask')[0].getContext('2d');
+
+  // mask.beginPath();
+  // mask.moveTo(0, 0);
+  // mask.lineTo(41, 0);
+  // mask.lineTo(33, 180);
+  // mask.lineTo(0, 180);
+  // mask.fillStyle = 'rgba(0, 0, 0, 0.5)';
+  // mask.fill();
+
+
+
+  $('#footer').on('click', 'a', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    $.get($this.attr('href')).done(function(result) {
+      $('.stand-pic').attr('src', '/data/pic/' + result.standPic);
+      $('.master-pic').attr('src', '/data/pic/' + result.masterPic);
+      $('.stand-name').text(result.standName);
+      $('.master-name').text(result.masterName);
+      $this.parent().addClass('active').siblings().removeClass('active');
+      if(!stand) {
+        $('.stand-md').slideDown(function() {
+          $('.info-md img').addClass('active');
+          stand = new Stand({
+            r: 150,
+            height: 350,
+            width: $('#stand').width(),
+            centerX: 350,
+            centerY: 150,
+            data: result
+          });
+        });
+      } else {
+        $('.info-md img').toggleClass('active');
+        stand.update(result);
+      }
+    });
+
+  });
 
 });
 });
